828fb787b548f5bed76260be259e319951d6caf2
drew-chidi <chidi.andrew@gmail.com>
2023-04-27 14:21:40 +0100

    set up JWT, register and login user controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,26 +5,92 @@ const User = require('../models/userModel');
 // @desc Register new user
 // @route POST /api/users
 // @access Public
-
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: 'Register User' });
+  const { firstName, lastName, email, userName, password } = req.body;
+
+  if (!firstName || !lastName || !email || !userName || !password) {
+    res.status(400);
+    throw new Error('Please add all fields');
+  }
+
+  // Check if the user exist
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrpt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    userName,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      userName: user.userName,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // @desc Authenticate a user
 // @route POST /api/users/login
 // @access Public
-
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: 'Login User' });
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error('Please add all fields');
+  }
+
+  // Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      userName: user.userName,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid credentials');
+  }
 });
 
 // @desc Get user data
 // @route GET /api/users/me
-// @access Public
-
+// @access Private
 const getMe = asyncHandler(async (req, res) => {
   res.json({ message: 'User data display' });
 });
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
 
 module.exports = {
   registerUser,
--- models/userModel.js
+++ models/userModel.js
@@ -2,13 +2,22 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema(
   {
-    name: {
+    firstName: {
+      type: String,
+      required: [true, 'Please add a name'],
+    },
+    lastName: {
       type: String,
       required: [true, 'Please add a name'],
     },
     email: {
       type: String,
       required: [true, 'Please add a email'],
+      unique: true,
+    },
+    userName: {
+      type: String,
+      required: [true, 'Please add a name'],
       unique: true,
     },
     password: {
